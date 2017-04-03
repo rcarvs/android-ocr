29b2d3e5c4c5c2ffe69d877542fe6b1e1ba87a96
rcarvs <renan.carvalho.sousa@gmail.com>
2017-04-03 08:25:33 -0300

    TEstes pesados

--- app/src/main/obj/local/x86/objs/ParallelMERuntime/src/parallelme/Kernel.o.d
+++ app/src/main/obj/local/x86/objs/ParallelMERuntime/src/parallelme/Kernel.o.d
@@ -1,118 +1,118 @@
 ./obj/local/x86/objs/ParallelMERuntime/src/parallelme/Kernel.o: \
   jni/ParallelME/runtime/src/parallelme/Kernel.cpp \
   jni/ParallelME/runtime/include/parallelme/Kernel.hpp \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef___deallocate \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef_min_max \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef___deallocate \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef_min_max \
   jni/ParallelME/runtime/include/parallelme/Buffer.hpp \
   jni/ParallelME/runtime/include/parallelme/Device.hpp \
   jni/ParallelME/runtime/include/parallelme/Program.hpp \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/map \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tree \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/set \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/map \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tree \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional \
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/set \
   jni/ParallelME/runtime/src/parallelme/dynloader/dynLoader.h \
   jni/ParallelME/runtime/src/parallelme/dynloader/headers/cl.h \
   jni/ParallelME/runtime/src/parallelme/dynloader/headers/cl_platform.h \
-  /home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stddef.h
+  /home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stddef.h
 
 jni/ParallelME/runtime/include/parallelme/Kernel.hpp:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef___deallocate:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef___deallocate:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef_min_max:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__undef_min_max:
 
 jni/ParallelME/runtime/include/parallelme/Buffer.hpp:
 
@@ -120,13 +120,13 @@ jni/ParallelME/runtime/include/parallelme/Device.hpp:
 
 jni/ParallelME/runtime/include/parallelme/Program.hpp:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/map:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/map:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tree:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tree:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/set:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/set:
 
 jni/ParallelME/runtime/src/parallelme/dynloader/dynLoader.h:
 
@@ -134,4 +134,4 @@ jni/ParallelME/runtime/src/parallelme/dynloader/headers/cl.h:
 
 jni/ParallelME/runtime/src/parallelme/dynloader/headers/cl_platform.h:
 
-/home/labpi/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stddef.h:
+/home/rcarvs/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stddef.h:
